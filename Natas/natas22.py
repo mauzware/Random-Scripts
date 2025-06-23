ceb8488028f5258bf284ea38481333ba806e1a3f
mauzinho <mauzware@gmail.com>
2025-06-23 17:43:10 +0200

    natas22.py

--- Natas/natas22.py
+++ Natas/natas22.py
@@ -1,7 +1,7 @@
 import requests
 
 url = 'http://natas22.natas.labs.overthewire.org/?revelio=1'
-auth = ('natas22', 'd8rwGBl0Xslg3b76uh3fEbSlnOUBlozz')  
+auth = ('natas22', '[PW_HERE]')  
 
 # Just hit it and stop redirecting
 r = requests.get(url, auth=auth, allow_redirects=False)
